c308cb49f3cb9cdd2d5083c63f19b514e0aaafc1
1PabloL22 <148669398+1PabloL22@users.noreply.github.com>
2023-10-22 00:32:28 -0300

    Add files via upload

---
+++ Algoritmos e Lógica de Programação - C++.cpp
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Funcao que ira efetuar os calculos de Collatz
+
+ Regra:
+  if (n % 2 == 0) -> n / 2
+  else -> (3*n) + 1
+
+
+ Essa funcao nao precisa retornar nenhum valor, por isso ela é void.
+
+ OBS: O metodo utilizado para solucionar esse problema foi iterativo,
+      onde fazemos calculos em um laco de repeticao ate que a condicao de parada seja satisfeita.
+*/
+
+void collatz(int n) {
+    printf("\nResultado: \n\n\n");
+
+    /* Enquanto o resultado das operacoes for maior que 1, significa que ainda temos que
+       calcular o proximo termo para satisfazer a conjuctura de Collatz
+    */
+    while (n > 1) {
+
+        // Se for par
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            // Se for impar
+            n = (3 * n) + 1;
+        }
+
+        // Imprimindo o valor atualizado a cada iteracao
+        printf("%d ", n);
+    }
+}
+
+int main(void) {
+    // Essa variavel sera responsavel por conter o valor do termo que será calculado em breve
+    int userInput;
+
+    // Mensagem de apresentacao do programa
+    printf("\nInforme um numero para o calculo: ");
+
+    // Recebendo input do usuario
+    scanf("%d", &userInput);
+
+    // Chamando a funcao
+    collatz(userInput);
+
+    // Encerrando o programa
+    return 0;
+}
